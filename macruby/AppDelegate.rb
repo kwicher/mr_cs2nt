096933899de9a86ada207d842c984bbf613ad10a
Krzysztof B Wicher <kwicher@gmail.com>
2012-01-11 01:35:19 +0000

    Initial Commit

---
+++ macruby/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  macruby
+#
+#  Created by Krzysztof Wicher on 11/01/2012.
+#  Copyright 2012 MiK. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
